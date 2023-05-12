7501d35e42964009d57c2b553ce7c74d2a2f6005
paulopariz <paulopariz01@gmail.com>
2023-05-12 00:03:26 -0300

    Arrays e Tuplas | curso-2

--- curso-2/main.ts
+++ curso-2/main.ts
@@ -1,6 +1,19 @@
+//TIPOS BASICOS E INFERENCIA DE TIPOS
 let nome: string
 nome = "paulo"
 
 let idade = 19
 // idade = "abs"
 idade = 20
+
+
+
+
+// ARRAYS E TUPLAS
+let lista: string[]
+lista = ["abc", "123"]
+
+
+let aluno: [string, number, boolean]
+// aluno = [true, "paulo", 19]
+aluno = ["paulo", 19, true]
