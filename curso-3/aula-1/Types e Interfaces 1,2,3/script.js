e13000befcde8a6135314ab82b7fb0a42d9205bb
paulopariz <paulopariz01@gmail.com>
2023-05-15 17:16:05 -0300

    Types e interfaces part3(exercicio) | aula-1 e course-3

--- curso-3/aula-1/Types e Interfaces 1,2,3/script.js
+++ curso-3/aula-1/Types e Interfaces 1,2,3/script.js
@@ -4,13 +4,13 @@
 let total1 = 10;
 total1 = '200';
 function preencherDados(dados) {
-    document.body.innerHTML += `
-  <div>
-    <h2>${dados.nome}</h2>
-    <p>R$ ${dados.preco}</p>
-    <p>Inclui teclado: ${dados.teclado ? 'sim' : 'não'}</p>
-  </div>
-  `;
+    //     document.body.innerHTML += `
+    //   <div>
+    //     <h2>${dados.nome}</h2>
+    //     <p>R$ ${dados.preco}</p>
+    //     <p>Inclui teclado: ${dados.teclado ? 'sim' : 'não'}</p>
+    //   </div>
+    //   `;
 }
 preencherDados({
     nome: 'Computador',
@@ -34,3 +34,25 @@ function pintarCategoria(categoria) {
     }
 }
 pintarCategoria('codigo');
+//EXERCICIO
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    showProduct(data);
+    console.log(data);
+}
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
+      <section>
+        <h2>${data.nome}</h2>
+        <p>${data.descricao}</p>
+        <p>R$ ${data.preco}</p>
+        <p>${data.seguroAcidentes}</p>
+        <h2>Empresa Fabricante: ${data.empresaFabricante.nome}</h2>
+        <p>Pais: ${data.empresaFabricante.pais}</p>
+        <h2>Empresa Montadora: ${data.empresaMontadora.nome}</h2>
+        <p>Pais: ${data.empresaMontadora.pais}</p>
+        </section>
+    `;
+}
--- curso-3/aula-1/Types e Interfaces 1,2,3/script.ts
+++ curso-3/aula-1/Types e Interfaces 1,2,3/script.ts
@@ -20,13 +20,13 @@ type TypeProduto = {
 
 
 function preencherDados(dados: TypeProduto) {
-    document.body.innerHTML += `
-  <div>
-    <h2>${dados.nome}</h2>
-    <p>R$ ${dados.preco}</p>
-    <p>Inclui teclado: ${dados.teclado ? 'sim' : 'não'}</p>
-  </div>
-  `;
+    //     document.body.innerHTML += `
+    //   <div>
+    //     <h2>${dados.nome}</h2>
+    //     <p>R$ ${dados.preco}</p>
+    //     <p>Inclui teclado: ${dados.teclado ? 'sim' : 'não'}</p>
+    //   </div>
+    //   `;
 }
 
 preencherDados({
@@ -56,3 +56,47 @@ function pintarCategoria(categoria: Categorias) {
 }
 
 pintarCategoria('codigo');
+
+
+
+
+//EXERCICIO
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    showProduct(data);
+    console.log(data)
+}
+
+fetchProduct();
+
+interface Empresa {
+    fundacao: number;
+    nome: string;
+    pais: string;
+}
+
+interface Produtos {
+    nome: string;
+    garantia: string;
+    preco: number;
+    seguroAcidentes: boolean;
+    descricao: string;
+    empresaFabricante: Empresa
+    empresaMontadora: Empresa
+}
+
+function showProduct(data: Produtos) {
+    document.body.innerHTML = `
+      <section>
+        <h2>${data.nome}</h2>
+        <p>${data.descricao}</p>
+        <p>R$ ${data.preco}</p>
+        <p>${data.seguroAcidentes}</p>
+        <h2>Empresa Fabricante: ${data.empresaFabricante.nome}</h2>
+        <p>Pais: ${data.empresaFabricante.pais}</p>
+        <h2>Empresa Montadora: ${data.empresaMontadora.nome}</h2>
+        <p>Pais: ${data.empresaMontadora.pais}</p>
+        </section>
+    `;
+}
