5009f3ce87c2f2197280ccc2ffb0c385df0d878f
paulopariz <paulopariz01@gmail.com>
2023-05-14 23:00:22 -0300

    string, number e boolean | aula-1 e course-3

--- curso-3/aula-1/String, Number, e Boolean/script.ts
+++ curso-3/aula-1/String, Number, e Boolean/script.ts
@@ -1,2 +1,25 @@
-let nome = 'PAULO'
-console.log(nome)
+//typeof
+//typeof é um operador de JavaScript que retorna uma string indicando o tipo do dado. Os possíveis valores retornados por typeof são: string, number, boolean, function, object, undefined, bigint e symbol. O typeof funciona como um type guard.
+
+
+// const frase = 'Front End';
+// const preco = 500;
+// const condi = preco > 100;
+
+// if (typeof frase === 'string') {
+//   console.log('frase é string');
+// }
+// if (typeof preco === 'number') {
+//   console.log('preco é number');
+// }
+// if (typeof condi === 'boolean') {
+//   console.log('condi é boolean');
+// }
+
+const frase1 = new String('Front End');
+const frase2 = String('Front End');
+const frase3 = 'Front End';
+
+console.log(typeof frase1);
+console.log(typeof frase2);
+console.log(typeof frase3);
