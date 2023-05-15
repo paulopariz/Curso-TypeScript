a797ca1f72a9efd96f134d0edb1c17b62cdd4c9c
paulopariz <paulopariz01@gmail.com>
2023-05-15 17:46:48 -0300

    Arrays part2(exercicio) | aula-1 e course-3

--- curso-3/aula-1/Arrays 1,2/script.js
+++ curso-3/aula-1/Arrays 1,2/script.js
@@ -13,3 +13,32 @@ const dados = [
     ['senhor dos aneis', 80],
     ['a guerra dos tronos', 120],
 ];
+//EXERCICIO
+async function fetchCursos() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const data = await response.json();
+    mostrarCursos(data);
+    console.log(data);
+}
+fetchCursos();
+function mostrarCursos(cursos) {
+    cursos.forEach(curso => {
+        let color;
+        if (curso.nivel === 'iniciante') {
+            color = 'blue';
+        }
+        else if (curso.nivel === 'avancado') {
+            color = 'red';
+        }
+        document.body.innerHTML += `
+        <section>
+            <h2 style="color: ${color};">${curso.nome}</h2>
+            <p>Horas: ${curso.horas}</p>
+            <p>Quantidade de aulas: ${curso.aulas}</p>
+            <p>Tipo: ${curso.gratuito ? "Gratuito" : "Pago"}</p>
+            <p>${curso.tags.join(", ")}</p>
+            <p>${curso.idAulas.join(" | ")}</p>
+        </section>
+        `;
+    });
+}
