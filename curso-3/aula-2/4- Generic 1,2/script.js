d448a40c23ca67bf415b818737fd53039e55b11f
paulopariz <paulopariz01@gmail.com>
2023-05-16 15:38:34 -0300

    Generic part2 | aula-2 e course-3

--- curso-3/aula-2/4- Generic 1,2/script.js
+++ curso-3/aula-2/4- Generic 1,2/script.js
@@ -33,3 +33,33 @@ function tipoDado(a) {
 tipoDado("teste").tipo;
 tipoDado(true).tipo;
 tipoDado(2000).tipo;
+//extends
+//É possível indicar que o tipo genérico deve herdar de uma interface específica com o extends.
+function extractText(el) {
+    return el.innerText;
+}
+const linkk = document.querySelector('a');
+if (linkk) {
+    console.log(extractText(linkk));
+    // extractText<HTMLAnchorElement extends HTMLElement>(el: HTMLAnchorElement): string
+}
+function $(selector) {
+    return document.querySelector(selector);
+}
+const link2 = $('a')?.href;
+//Métodos
+//Métodos nativos são definidos utilizando generics, assim podemos indicar durante a execução qual será o tipo esperado.
+const link3 = document.querySelector(".link");
+if (link3 instanceof HTMLVideoElement) {
+    link3?.volume;
+}
+async function getData(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+async function handleData() {
+    const notebook = await getData('https://api.origamid.dev/json/notebook.json');
+    console.log(notebook.nome);
+    console.log(notebook.preco);
+}
+handleData();
--- curso-3/aula-2/4- Generic 1,2/script.ts
+++ curso-3/aula-2/4- Generic 1,2/script.ts
@@ -50,3 +50,60 @@ function tipoDado<T>(a: T): { dado: T; tipo: string } {
 tipoDado("teste").tipo
 tipoDado(true).tipo
 tipoDado(2000).tipo
+
+
+
+
+//extends
+//É possível indicar que o tipo genérico deve herdar de uma interface específica com o extends.
+
+function extractText<Tipo extends HTMLElement>(el: Tipo): string {
+    return el.innerText;
+}
+
+const linkk = document.querySelector('a');
+
+if (linkk) {
+    console.log(extractText(linkk));
+    // extractText<HTMLAnchorElement extends HTMLElement>(el: HTMLAnchorElement): string
+}
+
+function $<Tipo extends Element>(selector: string): Tipo | null {
+    return document.querySelector(selector);
+}
+
+const link2 = $<HTMLAnchorElement>('a')?.href;
+
+
+
+
+
+
+//Métodos
+//Métodos nativos são definidos utilizando generics, assim podemos indicar durante a execução qual será o tipo esperado.
+
+const link3 = document.querySelector<HTMLAnchorElement>(".link")
+if (link3 instanceof HTMLVideoElement) {
+    link3?.volume
+}
+
+
+async function getData<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+interface Notebook {
+    nome: string;
+    preco: number;
+}
+
+async function handleData() {
+    const notebook = await getData<Notebook>(
+        'https://api.origamid.dev/json/notebook.json',
+    );
+    console.log(notebook.nome);
+    console.log(notebook.preco);
+}
+
+handleData()
