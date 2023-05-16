20c0cee013bf38e25c43a7528b152305c34abaa5
paulopariz <paulopariz01@gmail.com>
2023-05-16 17:37:04 -0300

    User Type Guard part1 | aula-2 e course-3

---
+++ curso-3/aula-2/8- User Type Guard 1,2,3/script.js
@@ -0,0 +1,31 @@
+"use strict";
+//Array
+//Uma array não pode ser verificada com o typeof pois a mesma é um object. Podemos verificar se o dado é instanceof Array ou podemos usar a função Array.isArray().
+async function fetchCursos2() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const json = await response.json();
+    handleCursos(json);
+}
+fetchCursos2();
+function handleCursos(data) {
+    if (data instanceof Array) {
+        console.log('É instância de Array');
+    }
+    if (Array.isArray(data)) {
+        console.log('É array');
+    }
+}
+//Type Predicate
+//TypeScript não executa JavaScript
+//Sabemos já que o TS não executa o JS durante a checagem dos tipos. Se isso ocorre, então como a função isArray consegue ser usada como Type Guard?
+//Com o Type Predicate :arg is type, podemos indicar qual o tipo de argumento uma função booleana (que retorna boolean) recebeu para ser verdadeira
+function isString(value) {
+    return typeof value === 'string';
+}
+function handleData(data) {
+    if (isString(data)) {
+        data.toUpperCase();
+    }
+}
+handleData('Origamid');
+handleData(200);
