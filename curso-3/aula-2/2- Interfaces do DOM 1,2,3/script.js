d0ba07bb73c5f65b5a7e2f9722bae71b66a83f98
paulopariz <paulopariz01@gmail.com>
2023-05-15 23:18:32 -0300

    Instanceof do DOM part2 | aula-2 e course-3

--- curso-3/aula-2/2- Interfaces do DOM 1,2,3/script.js
+++ curso-3/aula-2/2- Interfaces do DOM 1,2,3/script.js
@@ -5,3 +5,19 @@ const video = document.querySelector("#video");
 if (video instanceof HTMLVideoElement) {
     console.log(video.volume);
 }
+//querySelectorAll
+//O querySelectorAll retorna uma NodeList de elementos. Não confundir o nome da interface NodeListOf com o nome da classe NodeList.
+const links = document.querySelectorAll('.link');
+console.log(links instanceof NodeList);
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        console.log(link.href);
+    }
+    else {
+        console.log(typeof link);
+    }
+});
+// erro, filter é um método de Array e não de NodeList
+// const anchorLinks = links.filter((link) => link instanceof HTMLAnchorElement);
+// Funciona, pois a NodeList foi transformada em uma Array
+const anchorLinks = Array.from(links).filter((link) => link instanceof HTMLAnchorElement);
