34782fe2e1c0b53e3a5d54671caa59747a646103
paulopariz <paulopariz01@gmail.com>
2023-05-15 23:09:58 -0300

    Instanceof do DOM part1 | aula-2 e course-3

---
+++ curso-3/aula-2/2- Interfaces do DOM 1,2,3/script.ts
@@ -0,0 +1,7 @@
+//querySelector
+//Quando selecionamos um elemento do DOM com o querySelector, o objeto retornado dependerá da string que passarmos no método.
+
+const video = document.querySelector("#video")
+if (video instanceof HTMLVideoElement) {
+    console.log(video.volume)
+}
