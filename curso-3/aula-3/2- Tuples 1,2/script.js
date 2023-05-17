5ecdd37f9ae1ee5c1daa5eccf7724ba2902957d3
paulopariz <paulopariz01@gmail.com>
2023-05-17 15:46:33 -0300

    Tuples part2(exercicio) aula-3 | course-3

--- curso-3/aula-3/2- Tuples 1,2/script.js
+++ curso-3/aula-3/2- Tuples 1,2/script.js
@@ -21,3 +21,23 @@ const button5 = getText('button');
 if (button5) {
     const [buttonElement, buttonText] = button5;
 }
+//EXERCICIO
+async function fetchVendas() {
+    const response = await fetch('https://api.origamid.dev/json/vendas.json');
+    const data = await response.json();
+    somarVendas(data);
+}
+function somarVendas(vendas) {
+    // Com for loop
+    let total1 = 0;
+    for (let i = 0; i < vendas.length; i++) {
+        total1 += vendas[i][1];
+    }
+    document.body.innerHTML += `<p>Total: R$ ${total1}</p>`;
+    // Com reduce
+    const total2 = vendas.reduce((total, venda) => {
+        return total + venda[1];
+    }, 0);
+    document.body.innerHTML += `<p>Total: R$ ${total2}</p>`;
+}
+fetchVendas();
