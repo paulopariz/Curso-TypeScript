775ed6837ad630425ad6262b53389fac433c6168
paulopariz <paulopariz01@gmail.com>
2023-05-17 15:17:10 -0300

    Classes aula-3 | course-3

--- curso-3/aula-3/1- Classes/script.js
+++ curso-3/aula-3/1- Classes/script.js
@@ -1,1 +1,89 @@
 "use strict";
+//Classes 
+//As classes/funções construtoras são responsáveis pela construção de objetos que já vimos, como MouseEvent, HTMLElement e os demais.
+// f HTMLElement()
+console.log(document.constructor);
+class Produto5 {
+    tipo = 'produto';
+    nome;
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+const livro5 = new Produto5('O Senhor dos Anéis');
+//Modificadores
+//O TypeScript fornece diversas palavras-chave (Modifiers) que podem ser utilizadas em propriedades de classes modificar o comportamento/uso das mesmas.
+class Produto6 {
+    // public: é o padrão de qualquer uma
+    // private: só pode ser acessada dentro da classe
+    tipo = 'produto';
+    // protected: só pode ser acessada dentro da classe e subclasses
+    preco;
+    // readonly: só permite leitura
+    nome;
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.tipo;
+        this.preco = preco;
+    }
+    getTipo() {
+        return this.tipo;
+    }
+    getPreco() {
+        return Produto6.transformarPreco(this.preco);
+    }
+    // [javascript] static: não fará parte do Objeto criado
+    // e sim da função construtora (classe)
+    static transformarPreco(preco) {
+        return `R$ ${preco}`;
+    }
+}
+const livro6 = new Produto6('O Senhor dos Aneis', 200);
+console.log(livro6.getTipo());
+console.log(livro6.getPreco());
+console.log(livro6.nome);
+console.log(Produto6.transformarPreco(100));
+//Class e Interface
+//Ao definirmos uma classe, a sua interface é gerada automaticamente pelo TypeScript. A definição da classe é o que utilizamos para verificar se um objeto é uma instância da mesma instanceof class.
+class Quadrado {
+    lados = 4;
+    medida;
+    constructor(medida) {
+        this.medida = medida;
+    }
+    getPerimetro() {
+        return this.medida * this.lados;
+    }
+    getArea() {
+        return this.medida * this.medida;
+    }
+}
+class Circulo {
+    PI = Math.PI;
+    raio;
+    constructor(raio) {
+        this.raio = raio;
+    }
+    getPerimetro() {
+        return Math.round(2 * this.PI * this.raio * 100) / 100;
+    }
+    getArea() {
+        return Math.round(this.PI * (this.raio * this.raio) * 100) / 100;
+    }
+}
+const formas = [2, 32, 12, 3, 4, 20, 37, 9].map((n) => {
+    if (n < 15) {
+        return new Quadrado(n);
+    }
+    else {
+        return new Circulo(n);
+    }
+});
+formas.forEach((forma) => {
+    if (forma instanceof Quadrado) {
+        console.log(forma.getArea());
+    }
+    if (forma instanceof Circulo) {
+        console.log(forma.getPerimetro());
+    }
+});
