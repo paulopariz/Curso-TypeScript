ef081c6cc7229b32d41a4ed1eca911de95ce5613
paulopariz <paulopariz01@gmail.com>
2023-05-17 00:56:23 -0300

    Exercicio part2 | aula-2 e course-3

--- curso-3/aula-2/12- Exercicio 1,2/script.ts
+++ curso-3/aula-2/12- Exercicio 1,2/script.ts
@@ -11,6 +11,44 @@ interface Window {
 window.UserData = {}
 
 
+function isUserData(obj: unknown): obj is UserData {
+    if (obj && typeof obj === 'object' && ("nome" in obj || "email" in obj || "cpf" in obj)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function validJSON(str: string) {
+    try {
+        JSON.parse(str)
+    } catch (e) {
+        return false
+    }
+    return true
+}
+
+
+function loadLocalStorage() {
+    const localUserData = localStorage.getItem('UserData')
+    if (localUserData && validJSON(localUserData)) {
+        const UserData = JSON.parse(localUserData)
+        if (isUserData(UserData)) {
+            const retorno2 = Object.entries(UserData).forEach(([key, value]) => {
+                const input = document.getElementById(key)
+
+                if (input instanceof HTMLInputElement) {
+                    input.value = value
+
+                    window.UserData[key] = value
+                }
+            })
+        }
+    }
+}
+
+loadLocalStorage()
 
 function handleInput({ target }: KeyboardEvent) {
     if (target instanceof HTMLInputElement) {
