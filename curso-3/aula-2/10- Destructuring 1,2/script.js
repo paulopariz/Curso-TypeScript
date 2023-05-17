76bac10233f75c32800f510d4e3272ebe8d6a25f
paulopariz <paulopariz01@gmail.com>
2023-05-17 00:11:31 -0300

    Destructuring part2 | aula-2 e course-3

--- curso-3/aula-2/10- Destructuring 1,2/script.js
+++ curso-3/aula-2/10- Destructuring 1,2/script.js
@@ -18,3 +18,15 @@ function handleClickk2({ currentTarget, pageX, }) {
     }
 }
 document.documentElement.addEventListener('click', handleClickk2);
+//...rest
+//O operador ...rest retorna uma Array.
+function comparar(tipo, ...numeros) {
+    if (tipo === 'maior') {
+        return Math.max(...numeros);
+    }
+    if (tipo === 'menor') {
+        return Math.min(...numeros);
+    }
+}
+console.log(comparar('maior', 3, 2, 4, 30, 5, 6, 20));
+console.log(comparar('menor', 3, 2, 4, 1, 5, 6, 20));
--- curso-3/aula-2/10- Destructuring 1,2/script.ts
+++ curso-3/aula-2/10- Destructuring 1,2/script.ts
@@ -39,3 +39,22 @@ function handleClickk2({
 }
 
 document.documentElement.addEventListener('click', handleClickk2);
+
+
+
+
+
+//...rest
+//O operador ...rest retorna uma Array.
+
+function comparar(tipo: 'maior' | 'menor', ...numeros: number[]) {
+    if (tipo === 'maior') {
+        return Math.max(...numeros);
+    }
+    if (tipo === 'menor') {
+        return Math.min(...numeros);
+    }
+}
+
+console.log(comparar('maior', 3, 2, 4, 30, 5, 6, 20));
+console.log(comparar('menor', 3, 2, 4, 1, 5, 6, 20));
