257f52781b648f74ff39c3b2d2667336047f514d
paulopariz <paulopariz01@gmail.com>
2023-05-18 15:49:02 -0300

    Keyof part2 aula-3 | course-3

--- curso-3/aula-3/3- Keyof 1,2/script.js
+++ curso-3/aula-3/3- Keyof 1,2/script.js
@@ -18,3 +18,39 @@ function selecionar(seletor) {
     return document.querySelector(seletor);
 }
 selecionar('body');
+//checkInterface
+//O keyof pode ser utilizado para criarmos funções genéricas utilitárias.
+async function fetchData(url) {
+    const base = 'https://api.origamid.dev/json';
+    const response = await fetch(base + url);
+    return await response.json();
+}
+async function handleData5() {
+    const jogo = await fetchData('/jogo.json');
+    if (checkInterface(jogo, 'desenvolvedora')) {
+        console.log(jogo.desenvolvedora);
+    }
+    const livro = await fetchData('/livro.json');
+    if (checkInterface(livro, 'autor')) {
+        console.log(livro.autor);
+    }
+}
+handleData5();
+function checkInterface(obj, ...keys) {
+    if (obj &&
+        typeof obj === 'object' &&
+        keys.filter((key) => key in obj).length === keys.length) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function checkInterfaceSimples(obj, key) {
+    if (obj && typeof obj === 'object' && key in obj) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- curso-3/aula-3/3- Keyof 1,2/script.ts
+++ curso-3/aula-3/3- Keyof 1,2/script.ts
@@ -51,3 +51,69 @@ function selecionar<Chave extends keyof Elementos>(
 }
 
 selecionar('body');
+
+
+
+
+//checkInterface
+//O keyof pode ser utilizado para criarmos funções genéricas utilitárias.
+
+async function fetchData<T>(url: string): Promise<T> {
+    const base = 'https://api.origamid.dev/json';
+    const response = await fetch(base + url);
+    return await response.json();
+}
+
+interface Jogo {
+    nome: string;
+    ano: number;
+    desenvolvedora: string;
+    plataformas: string[];
+}
+
+interface Livro {
+    nome: string;
+    ano: number;
+    autor: string;
+    paginas: number;
+}
+
+async function handleData5() {
+    const jogo = await fetchData('/jogo.json');
+    if (checkInterface<Jogo>(jogo, 'desenvolvedora')) {
+        console.log(jogo.desenvolvedora);
+    }
+
+    const livro = await fetchData('/livro.json');
+    if (checkInterface<Livro>(livro, 'autor')) {
+        console.log(livro.autor);
+    }
+}
+
+handleData5();
+
+function checkInterface<Interface>(
+    obj: unknown,
+    ...keys: Array<keyof Interface>
+): obj is Interface {
+    if (
+        obj &&
+        typeof obj === 'object' &&
+        keys.filter((key) => key in obj).length === keys.length
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkInterfaceSimples<Interface>(
+    obj: unknown,
+    key: keyof Interface,
+): obj is Interface {
+    if (obj && typeof obj === 'object' && key in obj) {
+        return true;
+    } else {
+        return false;
+    }
+}
