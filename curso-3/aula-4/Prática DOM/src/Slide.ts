ea4bd78b1ad69691370c31162cd6efeaa9ad9de1
paulopariz <paulopariz01@gmail.com>
2023-05-22 17:06:26 -0300

    Video Element aula-4 | course-3

--- curso-3/aula-4/Prática DOM/src/Slide.ts
+++ curso-3/aula-4/Prática DOM/src/Slide.ts
@@ -26,7 +26,7 @@ export default class Slide {
         this.timeout = null
         this.pausedTimeout = null
 
-        this.index = 0
+        this.index = localStorage.getItem("activeSlide") ? Number(localStorage.getItem("activeSlide")) : 0;
         this.slide = this.slides[this.index]
 
         this.paused = false
@@ -36,15 +36,38 @@ export default class Slide {
 
     hide(el: Element) {
         el.classList.remove("active")
+        if (el instanceof HTMLVideoElement) {
+            el.currentTime = 0
+            el.pause()
+        }
     }
 
     show(index: number) {
         this.index = index
         this.slide = this.slides[this.index]
+
+
+        localStorage.setItem("activeSlide", String(this.index))
+
+
         this.slides.forEach((el) => this.hide(el))
         this.slide.classList.add("active")
 
-        this.auto(this.time)
+        if (this.slide instanceof HTMLVideoElement) {
+            this.autoVideo(this.slide)
+        } else {
+            this.auto(this.time)
+        }
+    }
+
+    autoVideo(video: HTMLVideoElement) {
+        video.muted = true
+        video.play()
+        let firstPlay = true
+        video.addEventListener("playing", () => {
+            if (firstPlay) this.auto(video.duration * 1000)
+            firstPlay = false
+        })
     }
 
     auto(time: number) {
@@ -70,6 +93,7 @@ export default class Slide {
         this.timeout?.pause()
         this.pausedTimeout = new Timeout(() => {
             this.paused = true
+            if (this.slide instanceof HTMLVideoElement) this.slide.pause()
         }, 300)
 
     }
@@ -78,6 +102,7 @@ export default class Slide {
         if (this.paused) {
             this.paused = false
             this.timeout?.continue()
+            if (this.slide instanceof HTMLVideoElement) this.slide.play()
         }
     }
 
