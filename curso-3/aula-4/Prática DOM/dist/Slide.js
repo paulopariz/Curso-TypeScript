8cb7f983fbf375b28918077bee97be3d4668685b
paulopariz <paulopariz01@gmail.com>
2023-05-22 15:41:20 -0300

    Show Slide aula-4 | course-3

--- curso-3/aula-4/Prática DOM/dist/Slide.js
+++ curso-3/aula-4/Prática DOM/dist/Slide.js
@@ -1,17 +1,27 @@
 export default class Slide {
     container;
-    elements;
+    slides;
     controls;
     time;
-    constructor(container, elements, controls, time = 5000) {
+    index;
+    slide;
+    constructor(container, slides, controls, time = 5000) {
         this.container = container;
-        this.elements = elements;
+        this.slides = slides;
         this.controls = controls;
         this.time = time;
-        console.log(this.container);
-        console.log(this.elements);
-        console.log(this.controls);
-        console.log(this.time);
+        this.index = 0;
+        this.slide = this.slides[this.index];
+        this.show(this.index);
+    }
+    hide(el) {
+        el.classList.remove("active");
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        this.slides.forEach((el) => this.hide(el));
+        this.slide.classList.add("active");
     }
 }
 //# sourceMappingURL=Slide.js.map
