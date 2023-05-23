3e18645b713a518698cfc0efcac07bf3119b6d40
paulopariz <paulopariz01@gmail.com>
2023-05-23 16:40:44 -0300

    Tipos basicos | curso-4

--- curso-4/dist/index.js
+++ curso-4/dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
-let age = 2;
+// TIPOS BASICOS
+let age = 19;
+const firstName = "Paulo";
+let isValid = true;
+//ERRO
+//O tipo 'number' não pode ser atribuído ao tipo 'boolean'.ts(2322)
+//isValid = 30
+isValid = false;
+let idk = 5;
+idk = "lscpsdc";
+idk = true;
+const ids = [1, 2, 3, 4, 5, 6, 7];
+const booleans = [true, true, false, true, false];
+const names = ["Paulo", "Maria", "João"];
--- curso-4/src/index.ts
+++ curso-4/src/index.ts
@@ -1,1 +1,19 @@
-let age: number = 2
+// TIPOS BASICOS
+
+let age: number = 19
+const firstName = "Paulo"
+
+let isValid: boolean = true
+//ERRO
+//O tipo 'number' não pode ser atribuído ao tipo 'boolean'.ts(2322)
+//isValid = 30
+isValid = false
+
+let idk: any = 5
+idk = "lscpsdc"
+idk = true
+
+
+const ids: number[] = [1, 2, 3, 4, 5, 6, 7]
+const booleans: boolean[] = [true, true, false, true, false]
+const names: string[] = ["Paulo", "Maria", "João"]
